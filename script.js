7df0edf3ae3f544d5f6657ddad1344df986a60e4
grhabriel <ghx.barbosahg@gmail.com>
2021-11-26 22:09:23 -0300

    Comit do site versão 1.0

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ botaoEsquerda.addEventListener('click',clicar);
 function clicar(){
     cont++;
     if(cont%2==1){
-        texto.innerHTML = "Experimente nosso delicioso café com pitadas de canela";
+        texto.innerHTML = "Experimente nosso delicioso café com raspa de canela em pau! Já disponível em todas as nossas unidades. Aproveite a promoção e não perca tempo!";
     }
     else{
-        texto.innerHTML = "Temos novos cafes disponiveis! Corra e não perca esta oportunidade! Por tempo ilimitado"
+        texto.innerHTML = "Novos grãos e novos cafés já estão disponíveis em nossas lojas, não deixe de conferir! Aproveite e peça aquele cappuccino delicioso que só nós temos!";
     }
-                //   bannner/banner0.jpg
+                //   bannner/banner0.jpg ou bannner/banner1.jpg
     let banner = `bannner/banner${cont%2}.jpg`;
     console.log(cont%2)
     bannerImg.setAttribute('src',banner);
